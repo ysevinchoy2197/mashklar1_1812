718ec16f939f8dc7e635f58d352b028b225907ee
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2025-12-18 21:50:52 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# #1-misol
+# x = 5
+# y = x * 2
+# print(y - 3)
+
+# #2-misol
+# a = 10
+# if a > 5:
+#     print("A")
+# else:
+#     print("B")
+
+# #3-misol
+# son = input("Son kiriting: ")
+# print(son + 5)
+
+# #4-misol
+# son = int(input())
+# if son % 2 == 0:
+#     print("Juft")
+# else:
+#     print("Toq")
+
+# #5-misol
+# sonlar = [4, 7, 1, 8, 2]
